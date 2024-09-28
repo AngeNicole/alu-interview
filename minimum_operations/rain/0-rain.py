a9df6a7034d64e0eae43a8ba8063b18d3a3457a0
AngeNicole <angenicolemukundwa22@gmail.com>
2024-09-28 22:25:27 +0400

    rain fix

--- minimum_operations/rain/0-rain.py
+++ minimum_operations/rain/0-rain.py
@@ -3,16 +3,28 @@
 
 
 def rain(walls):
-    """calculates how much rain will be trapped after it rains
-    """
-    if not walls or len(walls) < 3:
+    """Rain function"""
+    water_retained = 0
+
+    if not isinstance(walls, list) or len(walls) < 3:
         return 0
 
-    rain = 0
-    for i in range(1, len(walls) - 1):
-        left = max(walls[:i])
-        right = max(walls[i + 1:])
-        min_wall = min(left, right)
-        if walls[i] < min_wall:
-            rain += min_wall - walls[i]
-    return rain
+    # The left and right max at each index
+    length = len(walls)
+    left, right = [[0]*length, [0]*length]
+
+    # Compute the values of the those maxes
+    left[0] = walls[0]
+    for i in range(1, length):
+        left[i] = max(left[i-1], walls[i])
+
+    right[length-1] = walls[length-1]
+    for i in range(length-2, -1, -1):
+        right[i] = max(right[i + 1], walls[i])
+
+    # Compute the water retained at each index
+    # using the min of the left and right max
+    for i in range(len(walls)):
+        water_retained += min(left[i], right[i]) - walls[i]
+
+    return water_retained
